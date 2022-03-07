a04de3217d10fffb64744183c8616d946945ea22
madhureddy <madhureddy@websynergies.in>
2022-03-07 07:47:05 +0530

    App impl

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import STYLE_EXAMPLE from "./components/styles_example";
+import { ReactComponent as CloseIcon } from "./styles/close_icon.svg";
+import { ReactComponent as FraudIllustration } from "./styles/Fraud_illustration.svg";
+import { ReactComponent as Icon1 } from "./styles/1_icon.svg";
+import { ReactComponent as Icon2 } from "./styles/2_icon.svg";
+import { ReactComponent as ActiveCheckBox } from "./styles/active_checkbox.svg";
 
-function App() {
+import { ReactComponent as ContinueBtn } from "./styles/continue_btn.svg";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    
+    <div>
+        <header className="header_navigation">
+        <h3 className="close_btn">
+          <CloseIcon />
+        </h3>
+        <h1 className="scam_label">Scams are raising... </h1>
       </header>
+
+       <div className="fraud_illustration_base">
+        <FraudIllustration className="fraud_illustration" />
+      </div>
+       <div className="main_label">
+        <b>
+          Check that you did not log in <br /> from a fake dbs page!
+        </b>
+      </div>
+      <div className="check_instruction_outer">
+        <div className="check_instruction_1">
+          <Icon1 className="frame1_icon" />
+          <div className="frame1_content">
+            <b>Never log in from links in emails or SMS,</b>
+            <br />
+            even if they appear to be sent by DBS.
+          </div>
+        </div>
+        <div className="check_instruction_2">
+          <Icon2 className="frame1_icon" />
+          <div className="frame1_content">
+            <b>Check the page link </b>you are now using to <br /> access
+            digibank online:
+          </div>
+        </div>
+      </div>
+      <div className="ib_link_outer">
+        <div className="ib_link_inner">
+          <span className="ib_link_header">Only this link is authentic</span>
+          <div className="ib_link_active_checkbox_link">
+            <ActiveCheckBox className="ib_link_active_checkbox_icon" />
+            <b className="ib_link_active_checkbox_text">
+              internet-banking.dbs.com.sg
+            </b>
+          </div>
+        </div>
+      </div>
+      <div>
+      <div className="only_continue">
+        Only continue if you’re sure the link is correct. <br/>If it looks suspicious, cancel now.
+          </div>
+          <button className="continue_btn">Continue</button>
+          <button className="cancel_btn">Cancel</button>
+      </div>
     </div>
   );
 }
---
+++ src/components/styles_example.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "../styles/styles.css"
+
+function STYLE_EXAMPLE() {
+  return (
+    <div>
+        <h1 className='primary'>Do Something Quick</h1>
+    </div>
+  )
+}
+
+export default STYLE_EXAMPLE
